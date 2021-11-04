01255705cdf8a91fe151c90ab712f16b2395fcd6
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-04 16:05:33 +0200

    SendGrid, IOptions, registration email confirmation

--- Teamy/Server/Services/AzureBlobStorage.cs
+++ Teamy/Server/Services/AzureBlobStorage.cs
@@ -1,8 +1,15 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Options;
 
 namespace Teamy.Server.Services
 {
+    public class BlobStorageOptions
+    {
+        public string ConnectionString { get; set; }
+        public string ImagesContainer { get; set; }
+    }
+
     public interface IStorageService
     {
         Task<Uri> Upload(Stream fileStream, string fileName);
@@ -11,24 +18,22 @@ namespace Teamy.Server.Services
 
     public class AzureBlobStorageService : IStorageService
     {
-        readonly string connectionString;
-        readonly string containerName;
-        public AzureBlobStorageService(IConfiguration configuration)
+        BlobStorageOptions _options;
+        public AzureBlobStorageService(IOptions<BlobStorageOptions> options)
         {
-            this.connectionString = configuration.GetConnectionString("AzureImageStore");
-            this.containerName = configuration.GetSection("Storage:ImagesContainer").Value;
+            _options = options.Value;
         }
 
         public async Task<Uri> Upload(Stream fileStream, string fileName)
         {
-            BlobServiceClient blobServiceClient = new(connectionString);
-            BlobContainerClient container = blobServiceClient.GetBlobContainerClient(containerName); 
+            BlobServiceClient blobServiceClient = new(_options.ConnectionString);
+            BlobContainerClient container = blobServiceClient.GetBlobContainerClient(_options.ImagesContainer); 
             if(!container.Exists())
             {
-                await blobServiceClient.CreateBlobContainerAsync(containerName, PublicAccessType.BlobContainer);
+                await blobServiceClient.CreateBlobContainerAsync(_options.ImagesContainer, PublicAccessType.BlobContainer);
             }
 
-            BlobClient blobClient = new(connectionString, containerName, fileName);
+            BlobClient blobClient = new(_options.ConnectionString, _options.ImagesContainer, fileName);
 
             await blobClient.UploadAsync(fileStream);
             return await Task.FromResult(blobClient.Uri);
@@ -36,7 +41,7 @@ namespace Teamy.Server.Services
 
         public async Task<bool> Delete(string fileName)
         {
-            BlobClient blobClient = new(connectionString, containerName, fileName);
+            BlobClient blobClient = new(_options.ConnectionString, _options.ImagesContainer, fileName);
             await blobClient.DeleteAsync(DeleteSnapshotsOption.IncludeSnapshots);
             return true;
         }
