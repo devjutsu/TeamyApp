0bfe781d7364bd371d18995184d34746560113ec
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-10 18:51:57 +0200

    Auto propose 2 dates when creating

--- Teamy/Server/Controllers/TemplatesController.cs
+++ Teamy/Server/Controllers/TemplatesController.cs
@@ -45,8 +45,10 @@ namespace Teamy.Server.Controllers
                 v.Id = null;
                 v.EventDate = DateTime.Today.AddDays(1).AddHours(18);
                 v.EventDateTo = DateTime.Today.AddDays(1).AddHours(20);
-                v.ProposedDates = new List<ProposedDateVM>() { new ProposedDateVM() 
-                { Date = DateTime.Today.AddDays(1).AddHours(18), DateTo = DateTime.Today.AddDays(1).AddHours(20) } };
+                v.ProposedDates = new List<ProposedDateVM>() { 
+                    new ProposedDateVM() { Date = DateTime.Today.AddDays(1).AddHours(18), DateTo = DateTime.Today.AddDays(1).AddHours(20) },
+                    new ProposedDateVM() { Date = DateTime.Today.AddDays(2).AddHours(18), DateTo = DateTime.Today.AddDays(2).AddHours(20) }
+                };
                 v.Polls?.ForEach(p =>
                 {
                     p.Id = null;
