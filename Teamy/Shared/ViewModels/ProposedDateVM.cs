133a2cc5033730471a2b7023903cb12dc858a2f6
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-10 23:51:02 +0200

    Date editing

--- Teamy/Shared/ViewModels/ProposedDateVM.cs
+++ Teamy/Shared/ViewModels/ProposedDateVM.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace Teamy.Shared.ViewModels
 {
-    public record ProposedDateVM
+    public class ProposedDateVM : ICloneable
     {
         public Guid Id { get; set; }
         public DateTime Date { get; set; }
         public DateTime DateTo { get; set; }
         public Guid? EventId { get; set; }
         public List<DateVoteVM> Votes { get; set; } = new List<DateVoteVM>();
+
+        public object Clone()
+        {
+            var item = (ProposedDateVM)this.MemberwiseClone();
+            item.Votes = this.Votes?.ToList() ?? new List<DateVoteVM>();
+            return item;
+        }
     }
 
     public class DateVoteVM
