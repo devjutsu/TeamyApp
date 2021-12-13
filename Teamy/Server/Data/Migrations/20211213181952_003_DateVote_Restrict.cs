446171d3d58c5e7cc02a798d43aa9ed7cbbee57f
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-13 20:23:00 +0200

    Migration fix

---
+++ Teamy/Server/Data/Migrations/20211213181952_003_DateVote_Restrict.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Teamy.Server.Data.Migrations
+{
+    public partial class _003_DateVote_Restrict : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
