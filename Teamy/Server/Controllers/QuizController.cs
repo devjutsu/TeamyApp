27b75532454bb37613e284d53d6fb23bbab153fc
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-14 20:21:18 +0200

    Calling Quiz controller

---
+++ Teamy/Server/Controllers/QuizController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Teamy.Server.Data;
+using Teamy.Server.Models;
+using Teamy.Shared.ViewModels;
+
+namespace Teamy.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuizController : ControllerBase
+    {
+        ILogger<QuizController> _logger { get; set; }
+        TeamyDbContext _db { get; set; }
+        private readonly IMapper _mapper;
+        public QuizController(ILogger<QuizController> logger,
+                                    TeamyDbContext db,
+                                    IMapper mapper)
+        {
+            _logger = logger;
+            _db = db;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Get")]
+        public async Task<QuizVM> Get([FromBody] string qCode)
+        {
+            return new QuizVM();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("PostAnswer")]
+        public async Task Post(QuizAnswerPostVM answer)
+        {
+            return;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Submit/{qCode}")]
+        public async Task Submit([FromQuery] string qCode, string userId)
+        {
+            return;
+        }
+    }
+}
---
+++ Teamy/Server/Models/Quiz.cs
@@ -0,0 +1,10 @@
+﻿namespace Teamy.Server.Models
+{
+    public class Quiz
+    {
+        public Guid Id { get; set; }
+        public List<string> Questions { get; set; }
+        public string? UserId { get; set; }
+        public string QCode { get; set; }
+    }
+}
