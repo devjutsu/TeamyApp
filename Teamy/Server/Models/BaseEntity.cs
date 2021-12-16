65399c031f74e14be1a337a9222114e03b0e8c72
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-16 23:56:32 +0200

    Base entity

---
+++ Teamy/Server/Models/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace Teamy.Server.Models
+{
+    public interface IDateCreated
+    {
+        DateTime DateCreated { get; set; }
+    }
+    
+    public interface IDateUpdated
+    {
+        DateTime DateUpdated { get; set; }
+    }
+
+    public class CreatedEntity : IDateCreated
+    {
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class BaseEntity : IDateCreated, IDateUpdated
+    {
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
