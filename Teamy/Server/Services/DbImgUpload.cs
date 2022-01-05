efb385dc3019fefe8390c3d9fc97aa6b6b4f2111
Davis Doro(WS) <davisdoro@outlook.com>
2022-01-05 09:40:11 +0200

    Added User Image to profile

---
+++ Teamy/Server/Services/DbImgUpload.cs
@@ -0,0 +1,34 @@
+﻿using Teamy.Server.Data;
+
+namespace Teamy.Server.Services
+{
+    public interface IUploadImages
+    {
+        Task<string> UploadImageAsync(IFormFile image);
+    }
+
+    public class DbImgUpload : IUploadImages
+    {
+        private ILogger<DbImgUpload> _logger;
+        private TeamyDbContext _db;
+        private IStorageService _storage;
+
+        public DbImgUpload(ILogger<DbImgUpload> logger,
+                                    TeamyDbContext db,
+                                    IStorageService storage)
+        {
+            _logger = logger;
+            _db = db;
+            _storage = storage;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile image)
+        {
+            var filename = Guid.NewGuid().ToString().Substring(0, 13);
+            var ext = Path.GetExtension(image.FileName);
+            var imageId = await _storage.Upload(image.OpenReadStream(), filename + ext);
+
+            return imageId.AbsoluteUri;
+        }
+    }
+}
